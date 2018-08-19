a6332214853d2fa775cb310f4612c2818c0846ad
Danny Firmin <u6611178@anu.edu.au>
2018-08-19 16:15:15 +1000

    Q5 Finished

--- app/src/main/java/me/dannyfeng/labtest1/Q5.java
+++ app/src/main/java/me/dannyfeng/labtest1/Q5.java
@@ -2,6 +2,9 @@ package me.dannyfeng.labtest1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Q5 extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class Q5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q5);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.greeting:
+                Toast.makeText(getApplicationContext(),
+                        "Hello", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
